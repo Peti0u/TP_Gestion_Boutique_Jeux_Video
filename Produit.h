76b3ada32565f4a052ded1892adfc663fd4fe4cc
Peti0u <quent1.vibrac@gmail.com>
2025-06-15 12:37:12 +0200

    Classe de base abstraite

---
+++ Produit.h
@@ -0,0 +1,27 @@
+#ifndef PRODUIT_H
+#define PRODUIT_H
+#include <iostream>
+#include "ErreurArgumentInvalide.h"
+
+
+class Produit {
+protected:
+    std::string nomProduit;
+    double prixBase;
+
+    Produit(std::string p_nomProduit, double p_prixBase);
+
+public:
+    std::string getNomProduit() const;
+    double getPrixBase() const;
+
+    void setPrixBase(double p_prixBase);
+
+    virtual double calculerPrixTTC() const = 0;
+    virtual void afficherDetailsProduit() const = 0;
+    virtual void setStock(int nouveauStock) = 0;
+
+    virtual ~Produit();
+};
+
+#endif
