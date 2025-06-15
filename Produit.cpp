2f7dba8fc4fb5c199574ce79a132eebefe1ba3d4
Peti0u <quent1.vibrac@gmail.com>
2025-06-15 12:39:06 +0200

    Classe de base abstraite (sauvegardee)

---
+++ Produit.cpp
@@ -0,0 +1,21 @@
+#include "Produit.h"
+
+Produit::Produit(std::string p_nomProduit, double p_prixBase) :
+	nomProduit(p_nomProduit) {
+	setPrixBase(p_prixBase);
+}
+
+std::string Produit::getNomProduit() const { return nomProduit; }
+
+double Produit::getPrixBase() const { return prixBase; }
+
+void Produit::setPrixBase(double p_prixBase) {
+	if (p_prixBase < 0) {
+		throw ErreurArgumentInvalide("Le stock ne peut pas etre negatif");
+	}
+	else {
+		prixBase = p_prixBase;
+	}
+}
+
+Produit::~Produit() {}
